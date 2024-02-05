516d9e584dddbd7c6d36c5875a019d87a031e73f
MercyTolla <mercyab@hotmail.no>
2024-02-05 13:08:38 +0100

    hanks seems to be correct now..

--- Cornish.py
+++ Cornish.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-a = 3       # Half width of the pipe
-b = 15/2    # Half depth of the pipe
-x = a       # Horizontal position (constant value for x)
-R = a/b     # Aspect ratio
+a = 15       # Half width of the pipe in cm
+b = 3        # Half depth of the pipe in cm
+x = a        # Horizontal position (constant value for x)
+R = a / b    # Aspect ratio
 
 # Function F(R)
 def F(R):
@@ -18,30 +18,43 @@ def T(U_avg):
     T = (3 * U_avg) / (2 * b**2 * F(R))
     return T
 
-# Function for the velocity field u(x, y)
-def u(x, y, a, b, T, terms=5):
-    result = - (32 * T * b**2) / np.pi**3 * (
-        np.sum([(np.cosh(n * np.pi * x / (2 * b)) / np.cosh(n * np.pi * a / (2 * b))) * np.cos(n * np.pi * y / (2 * b)) 
-                for n in range(1, terms + 1)])
-    ) + T * (b**2 - y**2)
+# # Function for the velocity field u(x, y) - USING CORNISH
+# def u_C(x, y, a, b, T, terms=5):
+#     result = - (32 * T * b**2) / np.pi**3 * (
+#         np.sum([(np.cosh(n * np.pi * x / (2 * b)) / np.cosh(n * np.pi * a / (2 * b))) * np.cos(n * np.pi * y / (2 * b)) 
+#                 for n in range(1, terms + 1)])
+#     ) + T * (b**2 - y**2)
 
-    return result  # The conversion factor is not needed here
+#     return result
+
+# Function for the velocity field u(x, y) - USING HANKS
+def u_H(x, y, a, b, T, terms=5):
+    lambda_j = lambda j: (2 * j - 1) * math.pi / 2
+    A_j = lambda j: ((-1)**(j + 1)) / (2 * j - 1)**3
+    eta = y / b
+    xi = x / a
+    result = T * b**2 * (1 - eta**2 - 32 / np.pi**3 * (np.sum([A_j(j) * np.cosh(lambda_j(j) * R * xi) / np.cosh(lambda_j(j) * R) * np.cos(lambda_j(j) * eta)
+                  for j in range(1, terms + 1)])))
+    return result
 
 # Define the range of y values
-y_range = np.linspace(-b, b, 20)
+y_range = np.linspace(-a / 100, a / 100, 20)
 
 # Set the value for U_avg (this will determine the shear stress T)
-U_avg = 0.2*100 #cm/s to m/s
+U_avg = 0.2  # m/s
 
 # Calculate T using the provided U_avg value
-T_value = T(U_avg)
+T_value = T(U_avg * 100)
 
 # Compute the velocity field for each point in the range of y (note: x is constant)
-velocity_values = [u(x, y_val, a, b, T_value, terms=5) for y_val in y_range]
+velocity_values = [u_H(x, y_val, a, b, T_value, terms=10) for y_val in y_range]
 
 # Plot the velocity field
 plt.plot(y_range, velocity_values)
-plt.xlabel('Vertical position, [m]')
-plt.ylabel('Velocity [m/s]')
+plt.xlabel('Vertical position, [cm]')
+plt.ylabel('Velocity [cm/s]')
 plt.title('Velocity as a Function of Vertical Position')
-#plt.show()
+plt.show()
+
+
+
