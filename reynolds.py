5184d64dc6a6e29c9573e45366558be996948972
MercyTolla <mercyab@hotmail.no>
2024-04-23 14:47:11 +0200

    fixed

--- reynolds.py
+++ reynolds.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.graph_objects as go
 
 def read_file(filename):
     df = pd.read_excel(filename)
@@ -13,8 +14,9 @@ def read_file(filename):
 
 #Calculate Reynolds number
 def Re(rho_m, avg_v, mu):
-    d = 0.15  # m
-    Re = rho_m * avg_v * d / mu
+    d = 0.15  #m
+    avg_v_m_s = avg_v / 600.0  # Convert l/min to m/s
+    Re = rho_m * avg_v_m_s * d / mu
     return Re
 
 #Test 1 water 
@@ -22,11 +24,54 @@ df_w = read_file("water_test3.xlsm")
 rho_w = 1000  #kg/m^3
 mu_w = 1.002 * 10**(-3)  #Pa*s
 
-# Convert "Flow meter [l/min]" column to numeric values
 avg_vw = pd.to_numeric(df_w["Flow meter [l/min]"], errors='coerce')  # Convert to numeric, handling errors
+avg_vw = avg_vw.dropna() #Filter out any NaN values
 
-# Filter out any NaN values
-avg_vw = avg_vw.dropna()
+#print(round(Re(rho_w, avg_vw, mu_w)))
 
-reynolds_numbers = Re(rho_w, avg_vw, mu_w)
-print(reynolds_numbers)
+#At time of erosion
+#print(round(Re(rho_w, ..., mu_w)))
+
+
+
+
+
+
+
+
+
+
+
+
+#Plot (Tviler på at vi trenger dette)
+
+# fig = go.Figure()
+
+# #Trace for flow velocity
+# fig.add_trace(
+#     go.Scatter(
+#         x=df.index,
+#         y=flow,
+#         mode='lines',
+#         name='Velocity [l/min]',
+#         line=dict(color='purple', width=1),  
+#         )
+#     )
+
+# fig.update_layout(
+#     title='Time vs Velocity',
+#     xaxis_title='Time [s]',
+#     yaxis_title='Velocity [l/min]',
+#     height=800,
+#     legend=dict(
+#         x=0,
+#         y=1,
+#     ),
+#     xaxis=dict(
+#     range=[0, 1000]
+            
+#     )
+# )
+
+# fig.show()
+ 
