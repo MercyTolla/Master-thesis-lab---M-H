80040436142d8763b2eed1fbcebd9736a6c3c998
MercyTolla <mercyab@hotmail.no>
2024-04-25 15:32:15 +0200

    added obm and fixed y

--- test_plot.py
+++ test_plot.py
@@ -1,38 +1,30 @@
 import pandas as pd 
 import plotly.graph_objects as go
-import numpy as np
+from plotly.subplots import make_subplots
 
-#Les output data fra testene
-#Test 1. Etanol - 2.Vann - 3. WBM - 4.OBM
 def read_file(filename):
     df = pd.read_excel(filename)
     
-    #for column in df.columns:
-    #    df[column] = df[column].apply(lambda x: str(x).replace(',', '.'))
-    
+    for column in df.columns:
+        df[column] = df[column].apply(lambda x: str(x).replace(',', '.'))
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+
     df.set_index('Time  1 - default sample rate [s]', inplace=True)
-    columns = df.columns
     
-    #Parametere: (Hva skal vi se på?)
-    flow = df["Flow meter [l/min]"]
-    df["Inlet cell [kg]"] = df["Inlet cell [kg]"].astype(str).astype(int) #Vekt inlet cell [g]
-    df["Outlet cell [kg]"] = df["Outlet cell [kg]"].astype(str).astype(int) #Vekt outlet cell [g]
-
-    fig = go.Figure()
-
-    # Trace for flow velocity
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    # Add traces
     fig.add_trace(
         go.Scatter(
             x=df.index,
             y=df['Flow meter [l/min]'],
             mode='lines',
             name='Velocity [l/min]',
-            line=dict(color='purple', width=1),
-            yaxis='y2'
-        )
+            line=dict(color='purple', width=2),
+        ),
+        secondary_y=False,
     )
     
-    # Trace for cell1
     fig.add_trace(
         go.Scatter(
             x=df.index,
@@ -40,11 +32,10 @@ def read_file(filename):
             mode='lines',
             name='Inlet cell [kg]',
             line=dict(color='blue', width=1),
-            yaxis='y'
-        )
+        ),
+        secondary_y=True,
     )
 
-    # Trace for cell2
     fig.add_trace(
         go.Scatter(
             x=df.index,
@@ -52,39 +43,31 @@ def read_file(filename):
             mode='lines',
             name='Outlet cell [kg]',
             line=dict(color='green', width=1),
-            yaxis='y'
-        )
+        ),
+        secondary_y=True,
     )
     
     # Update layout
     fig.update_layout(
         title='Time vs Velocity',
         xaxis_title='Time [s]',
-        yaxis_title='Weight [kg]',
-        yaxis=dict(title='Weight [kg]', overlaying='y', side='left'),  # Primary y-axis
-        yaxis2=dict(title='Velocity [l/min]', overlaying='y', side='right'),  # Secondary y-axis
+        #yaxis=dict(title='Velocity [l/min]', overlaying='y', side='left'),  
+        #yaxis2=dict(title='Weight [kg]', overlaying='y', side='right'),
         legend=dict(x=0, y=1),
         height=800,
-        xaxis=dict(range=[0, 1000])
     )
-
     
-    # Set tick values for x and y axes
-    x_tickvals = [0, 100, 200, 300, 400, 500, 600, 700]
-    x_ticktext = [str(val) for val in x_tickvals]
-    #y_tickvals = [0, 50, 100, 150, 200]
-    #y_ticktext = [str(val) for val in y_tickvals]
-    fig.update_xaxes(tickvals=x_tickvals, ticktext=x_ticktext)
-    #fig.update_yaxes(tickvals=y_tickvals, ticktext=y_ticktext)
+    fig.update_yaxes(title_text="Velocity [l/min]", secondary_y=False)
+    fig.update_yaxes(title_text="Weight [kg]", secondary_y=True)
+
+    #x_tickvals = [0, 100, 200, 300, 400]
+    #x_ticktext = [str(val) for val in x_tickvals]
+    #fig.update_xaxes(tickvals=x_tickvals, ticktext=x_ticktext)
 
     # Show the plot
     fig.show()
     print(df.head())
     print(df.dtypes)
 
-
-print(read_file("Data/water_test5.xlsm"))
-
-
-
-#Modellering, regresjon etc? 
+# Call the function with your filename
+read_file("Data/water_test5.xlsm")
